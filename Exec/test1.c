dc6ce040360dae27be23387758c556fdcdcf69a4
grenaud- <grenaud-@student.42lausanne.ch>
2022-11-24 10:33:22 +0100

    ajout premier test execution

--- Exec/test1.c
+++ Exec/test1.c
@@ -4,55 +4,45 @@
 
 int main(int argc, char *argv[], char *env[])
  {
-    char *args[][4]= {{"/bin/ls","-allll", NULL},
+    (void) argc;
+	(void) argv;
+	char *args[][4]= {{"/bin/ls","-allll", NULL},
     {"/usr/bin/grep", "mini", NULL},{"/usr/bin/wc", NULL} };
 
-    size_t i, n;
-    int prev_pipe, pfds[2];
-    n = sizeof(args) / sizeof(*args);
-    printf("n = %zu\n", n);
-    prev_pipe = STDIN_FILENO;
-
-    for (i = 0; i < n - 1; i++) {
-        pipe(pfds);
-
-        if (fork() == 0) {
-            // Redirect previous pipe to stdin
-            if (prev_pipe != STDIN_FILENO) {
-                dup2(prev_pipe, STDIN_FILENO);
-                close(prev_pipe);
-            }
-
-            // Redirect stdout to current pipe
-            dup2(pfds[1], STDOUT_FILENO);
-            close(pfds[1]);
-
-            // Start command
-            execve(args[i][0], args[i], env);
-
-            perror("execve failed");
-            exit(1);
-        }
-
-        // Close read end of previous pipe (not needed in the parent)
-        close(prev_pipe);
-
-        // Close write end of current pipe (not needed in the parent)
-        close(pfds[1]);
-
-        // Save read end of current pipe to use in next iteration
-        prev_pipe = pfds[0];
-    }
-
-    // Get stdin from last pipe
-    if (prev_pipe != STDIN_FILENO) {
-        dup2(prev_pipe, STDIN_FILENO);
-        close(prev_pipe);
-    }
-    //execve(args1[0], args1, env);
-    // Start last command
-     execve(args[i][0], args[i], env);
-
-    perror("execve failed");
-    exit(1);
+    int		i;
+	int		n;
+	int		prev_pipe;
+	int		pfds[2];
+	n = sizeof(args) / sizeof(*args);
+	prev_pipe = STDIN_FILENO;
+	i = 0;
+	while(i < n - 1)
+	{
+		pipe(pfds);
+		if (fork() == 0)
+		{
+			if (prev_pipe != STDIN_FILENO)
+			{
+				dup2(prev_pipe, STDIN_FILENO);
+				close(prev_pipe);
+			}
+			dup2(pfds[1], STDOUT_FILENO);
+			close(pfds[1]);
+			execve(args[i][0], args[i], env);
+			perror("execve failed");
+			exit(1);
+		}
+		close(prev_pipe);
+		close(pfds[1]);
+		prev_pipe = pfds[0];
+		i++;
+	}
+	if (prev_pipe != STDIN_FILENO)
+	{
+		dup2(prev_pipe, STDIN_FILENO);
+		close(prev_pipe);
+	}
+	execve(args[i][0], args[i], env);
+	perror("execve failed");
+	exit(1);
 }
