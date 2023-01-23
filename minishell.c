535c475f3db1e9bcb927c83e02d9aa4fbd59c3ac
grenaud- <grenaud-@student.42lausanne.ch>
2023-01-23 22:29:46 +0100

    leaks ok, reste a corriger avec les $

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: grenaud- <grenaud-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 17:43:28 by grenaud-          #+#    #+#             */
-/*   Updated: 2023/01/23 21:56:58 by grenaud-         ###   ########.fr       */
+/*   Updated: 2023/01/23 22:28:28 by grenaud-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,6 @@ char	*set_and_get(t_parser *p)
 int	main(int argc, char *argv[], char *env[])
 {
 	t_parser		p;
-	//struct termios	saved;
 
 	(void)argc;
 	(void)argv;
@@ -40,24 +39,12 @@ int	main(int argc, char *argv[], char *env[])
 	while (1)
 	{
 		p.line = set_and_get(&p);
- 		if (p.line == NULL)
-			break;
-		//printf("return val = %d g_status = %d\n", p.return_val, g_status);
-		//handle_signal(&saved);
+		if (p.line == NULL)
+			break ;
 		reload(&p, env);
-/* 		if (readline_manager(&p) == 1)
-			break ; */
 		parse_it(&p);
-		//printll(p.struct_cmd.cmd);
 		run_shell(&p);
-		//printf("resultat init_exe = %d", init_exe(&p));
-/* 		printf("\n\naddresse de p->struct_path.path = %p\n", p.struct_path.path);
-		printf("\n\naddresse de p->struct_cmd.cmd = %p\n", p.struct_cmd.cmd);
-		printf("\n\np-> empty = %d addresse de cmd_exe = %p\n\n", p.empty, p.cmd_exe); */
 		clean_memory_parsing(&p);
-/* 		printf("\n\np-> empty = %d addresse de cmd_exe = %p\n\n", p.empty, p.cmd_exe);
-		printf("\n\naddresse de p->struct_cmd.cmd = %p\n", p.struct_cmd.cmd);
-		printf("\n\naddresse de p->struct_path.path = %p\n", p.struct_path.path); */
 	}
 	delete_exeline(&p.cmd_exe);
 	clean_memory_end(&p);
